6669cfd5c3f1e61fc5f3dd9c6228894b08a97f83
Martin Folkeseth <martin.folkeseth@gmail.com>
2019-04-23 15:17:15 +0200

    Remove unused console log

--- scripts/transpile.js
+++ scripts/transpile.js
@@ -5,7 +5,6 @@ const RE_JS = /\.jsx?$/
 
 module.exports = async function transpile (filePath, isServer) {
   const jsTransforms = isServer ? ['imports', 'jsx'] : ['jsx']
-  console.log('match file?', RE_JS.test(filePath))
   if (RE_JS.test(filePath)) {
     return sucrase.transform(fs.readFileSync(filePath, 'utf8'), {
       transforms: jsTransforms
